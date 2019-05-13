b3ed85c99f94311f67b92946bc30778fc2968d5c
Alejandro Luque <aluque@iaa.es>
2019-05-13 10:56:47 +0200

    Add some utilities.

---
+++ util/plot_image.py
@@ -0,0 +1,83 @@
+import sys
+import scipy.constants as co
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
+from scipy import stats
+
+import h5py
+
+def get_parser():
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input",
+                        help="HDF5 input file1")
+
+    parser.add_argument("--observer",
+                        type=int,
+                        default=1,
+                        help="Observer number")
+
+    parser.add_argument("--xlim", "-x",
+                        help="Limits of the x-axis (x0:x1)", 
+                        action='store', default=None)
+
+    parser.add_argument("--ylim", "-y",
+                        help="Limits of the z-axis (y0:y1)", 
+                        action='store', default=None)
+
+    parser.add_argument("--log", action='store_true',
+                        help="Logarithmic scale?")
+
+
+    return parser
+
+
+def main():
+    parser = get_parser()                        
+    args = parser.parse_args()
+    obs = args.observer
+    
+    fp = h5py.File(args.input, "r")
+    
+    img = np.array(fp[f"obs{obs:05d}/image"])
+
+    plt.figure(f"obs{obs:05d}")
+
+    width, height = img.shape
+    x, y = np.arange(width), np.arange(height)
+    
+    kwargs = {}
+
+    if args.xlim is not None:
+        xlim = [float(v) for v in args.xlim.split(':')]
+        xfilter = np.logical_and(xlim[0] < x, x < xlim[1])
+        img = img[xfilter, :]
+        x = x[xfilter]
+        
+    if args.ylim is not None:
+        ylim = [float(v) for v in args.ylim.split(':')]
+        yfilter = np.logical_and(ylim[0] < y, y < ylim[1])
+        img = img[:, yfilter]
+        y = y[yfilter]
+
+    if args.log:
+        kwargs['norm'] = LogNorm()
+
+    plt.pcolormesh(x, y, img.T, **kwargs)
+    cbar = plt.colorbar()
+
+    plt.title(args.input)
+
+    plt.axis('scaled')
+    plt.xlabel("px")
+    plt.ylabel("py")
+
+    plt.show()
+
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ util/plot_photo.py
@@ -0,0 +1,112 @@
+import sys
+import scipy.constants as co
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import stats
+from scipy.optimize import curve_fit
+from scipy.integrate import trapz
+
+import h5py
+
+def get_parser():
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input",
+                        help="HDF5 input file1")
+    parser.add_argument("--observer",
+                        type=int,
+                        default=1,
+                        help="Observer number (1-based)")
+    return parser
+
+
+def main():
+    parser = get_parser()                        
+    args = parser.parse_args()
+
+    plotone(args.input, args.observer)
+        
+    
+def plotone(fname, obs):
+    fp = h5py.File(fname, "r")
+    
+    tl = np.array(fp[f"obs{obs:05d}/timeline"])
+    t = np.array(fp[f"obs{obs:05d}/t"])
+    g = fp[f"obs{obs:05d}"]
+
+    from IPython import embed
+    embed()
+
+    decay = decay_time(t - g.attrs["delay"], tl)
+
+    tshift = t - g.attrs["delay"]
+
+    plt.plot(tshift / co.milli,
+             tl, label="shift=%f" % g.attrs["shift"])
+
+    sigma, l0, z = lognorm_fit(tshift, tl)
+
+    # def lnorm(t, sigma, l0, A):
+    #     return A * np.exp(-(np.log(tshift) - l0)**2 / (2 * sigma**2))
+
+    # popt, pcov = curve_fit(lnorm, tshift, tl, p0=[sigma, l0, np.exp(z)])
+    # sigma, l0, A = popt
+
+    A = np.exp(z)
+    t0 = np.exp(l0)
+    itotal = A * np.sqrt(2 * np.pi) * np.exp(sigma**2 / 2) * t0 * sigma
+
+    dt = t[1] - t[0]
+    itotal0 = sum(tl) * dt
+
+    print(f"    sigma = {sigma}")
+    print(f"    t0 = {np.exp(l0) / co.milli} ms")
+    print(f"    A  = {A}")
+    print(f"    itotal  = {itotal} ({itotal0})")
+
+    plt.plot(tshift / co.milli,
+             A * np.exp(-(np.log(tshift) - l0)**2 / (2 * sigma**2)),
+             c='k', alpha=0.5, lw=0.75)
+
+    print("shift = %g km ==>  decay = %g ms" %
+          (g.attrs["shift"] / co.kilo,
+           decay / co.milli))
+        
+    plt.xlabel("Time (ms)")
+    plt.ylabel("photons / m$^2$ / s / source photon")
+    #plt.xlim([1e-2, 15])
+    #plt.loglog()
+    plt.show()
+
+    
+def decay_time(t, s):
+    imax = np.argmax(s)
+
+    t = t[imax:] - t[imax]
+    s = s[imax:] / s[imax]
+
+    flt = s > 0.1
+    a, b, _, _, _ = stats.linregress(t[flt], np.log(s[flt]))
+
+    return -1 / a
+
+
+def lognorm_fit(t, s):
+    imax = np.argmax(s)
+    
+    flt = np.logical_and(s > 0.05 * s[imax], t > 0)
+
+    v = np.vander(np.log(t[flt]), 3)
+    a, b, c = np.linalg.lstsq(v, np.log(s[flt]))[0]
+
+    sigma = 1 / np.sqrt(-2 * a)
+    l0 = -0.5 * b / a
+    z = c - 0.25 * b**2 / a
+
+    return sigma, l0, z
+    
+    
+if __name__ == '__main__':
+    main()
