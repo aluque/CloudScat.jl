de8015fbc4ea7d93da48e855553745f17233f9f7
Alejandro Luque <aluque@iaa.es>
2020-03-14 17:24:49 +0100

    Fix bug in VariableNR.

--- src/composition.jl
+++ src/composition.jl
@@ -98,7 +98,7 @@ end
 
 function VariableNR(λ, nrfetch, rmax, nmax; refindex=DEF_REFINDEX_FILE)
     g0, r0, a, c = miefitparams(λ, refindex)
-    Qext_max = 2. + comp.c * power34(rmax)
+    Qext_max = 2. + c * power34(rmax)
     νmiemax = Qext_max * π * rmax^2 * nmax
 end
 
